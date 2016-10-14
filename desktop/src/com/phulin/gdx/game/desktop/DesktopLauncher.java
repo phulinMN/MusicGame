a6a7ec9c329748321c1070fc53f6ff5d48acdf7c
phulin <pongpaiMN@gmail.com>
2016-10-14 10:49:49 +0700

    first commit

---
+++ desktop/src/com/phulin/gdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.phulin.gdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.phulin.gdx.game.MusicGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MusicGame(), config);
+	}
+}
