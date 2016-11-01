a8fd852e0f481b99a23456836150bce2580c9cbd
phulin <pongpaiMN@gmail.com>
2016-11-02 00:14:29 +0700

    FallDownTapPlay

--- core/src/com/phulin/gdx/game/Tabplay.java
+++ core/src/com/phulin/gdx/game/Tabplay.java
@@ -9,10 +9,12 @@ public class Tabplay {
 	private MusicGame musicGame;
 	private Texture tabplayImg;
 	private String [] Bar = new String[] {
-			"-----", "-***-", "*-*-*", "****-", "***-*"};
+			"-****", "*-***", "**-**", "***-*", "****-",
+			"*****", "*****", "*****", "-***-", "*-*-*"};
 	private int x = 0;
 	private int y;
 	private int k = 0;
+	static int SPEED = 4;
 	
 	public Tabplay(MusicGame musicGame){
 		this.musicGame = musicGame;
@@ -33,10 +35,10 @@ public class Tabplay {
 	public void render(float delta) {
 		SpriteBatch batch = musicGame.batch;
         batch.begin();
-    	for(int r = 0; r < 5; r++){
-    		for(int c = 4; c >= 0; c--){
+    	for(int r = 0; r < 10; r++){
+    		for(int c = 0; c < 5; c++){
     			x = (c*120);
-    			y = 760 - (r*40)-(k*5);
+    			y = 760 + r*40 - k*SPEED;
     			if(hasBarAt(r,c)){
     				batch.draw(tabplayImg, x, y);
     			}
