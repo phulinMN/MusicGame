80f9201f71bce86867a2b1505e3dc4e7c4675127
phulin <pongpaiMN@gmail.com>
2016-11-02 19:41:32 +0700

    CheckKey on Control tap

--- core/src/com/phulin/gdx/game/GameScreen.java
+++ core/src/com/phulin/gdx/game/GameScreen.java
@@ -3,6 +3,8 @@ package com.phulin.gdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,48 +15,51 @@ public class GameScreen extends ScreenAdapter {
 	private WorldRenderer worldRenderer;
 	private Tabplay tabplay;
 	private int x;
-	private int y;
-	private boolean isClick = true;
+	private Music babab;
 	
 	public GameScreen(MusicGame musicGame){
 		this.musicGame = musicGame;
 		tapImg = new Texture("tapb.png");
 		worldRenderer = new WorldRenderer(musicGame);
 		tabplay = new Tabplay(musicGame);
+		Music music = Gdx.audio.newMusic(Gdx.files.internal("twinkle.mp3"));
+		music.play();
 	}
 	
 	public void ControlKey(float delta){
-		if(Gdx.input.isKeyPressed(Keys.D) && !isClick){
-			isClick = true;
-			System.out.println("D");
-		}
-		if(Gdx.input.isKeyPressed(Keys.D) && isClick){
-			isClick = false;
-		}
-		if(Gdx.input.isKeyPressed(Keys.F) && !isClick){
-			System.out.println("F");
-		}
-		if(Gdx.input.isKeyPressed(Keys.F) && isClick){
-			isClick = false;
-		}
-		if(Gdx.input.isKeyPressed(Keys.SPACE) && !isClick){
-			System.out.println("SPACE");
-		}
-		if(Gdx.input.isKeyPressed(Keys.SPACE) && isClick){
-			isClick = false;
-		}
-		if(Gdx.input.isKeyPressed(Keys.J) && !isClick){
-			System.out.println("J");
-		}
-		if(Gdx.input.isKeyPressed(Keys.J) && isClick){
-			isClick = false;
-		}
-		if(Gdx.input.isKeyPressed(Keys.K) && !isClick){
-			System.out.println("K");
-		}
-		if(Gdx.input.isKeyPressed(Keys.K) && isClick){
-			isClick = false;
-		}
+    		if(Gdx.input.isKeyPressed(Keys.D)){
+    			tabplay.D_ISPRESS = true;
+    		}
+    		if(!Gdx.input.isKeyPressed(Keys.D)){
+    			tabplay.D_ISPRESS = false;
+    		}
+    		if(Gdx.input.isKeyPressed(Keys.F)){
+    			tabplay.F_ISPRESS = true;
+    		}
+    		if(!Gdx.input.isKeyPressed(Keys.F)){
+    			tabplay.F_ISPRESS = false;
+    		}
+    		if(Gdx.input.isKeyPressed(Keys.SPACE)){
+    			tabplay.SPACE_ISPRESS = true;
+    		}
+    		if(!Gdx.input.isKeyPressed(Keys.SPACE)){
+    			tabplay.SPACE_ISPRESS = false;
+    		}
+    		if(!Gdx.input.isKeyPressed(Keys.K)){
+    			tabplay.K_ISPRESS = false;
+    		}
+    		if(Gdx.input.isKeyPressed(Keys.J)){
+    			tabplay.J_ISPRESS = true;
+    		}
+    		if(!Gdx.input.isKeyPressed(Keys.J)){
+    			tabplay.J_ISPRESS = false;
+    		}
+    		if(Gdx.input.isKeyPressed(Keys.K)){
+    			tabplay.K_ISPRESS = true;
+    		}
+    		if(!Gdx.input.isKeyPressed(Keys.K)){
+    			tabplay.K_ISPRESS = false;
+    		}
 	}
 		
 	@Override
@@ -63,12 +68,10 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		SpriteBatch batch = musicGame.batch;
 		worldRenderer.render(delta);
-		x = 0;
-        y = 40;
         batch.begin();
         for(int i = 0; i < 5; i++){
         	x = (i*120);
-        	batch.draw(tapImg,x,y);
+        	batch.draw(tapImg,x,40);
         }
         batch.end();
         tabplay.render(delta);
--- core/src/com/phulin/gdx/game/Tabplay.java
+++ core/src/com/phulin/gdx/game/Tabplay.java
@@ -1,6 +1,7 @@
 package com.phulin.gdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,12 +10,54 @@ public class Tabplay {
 	private MusicGame musicGame;
 	private Texture tabplayImg;
 	private String [] Bar = new String[] {
-			"-****", "*-***", "**-**", "***-*", "****-",
-			"*****", "*****", "*****", "-***-", "*-*-*"};
-	private int x = 0;
-	private int y;
+			"*-***",
+			"-****",
+			"**-**",
+			"***-*",
+			"****-",
+			"*-***",
+			"*****",
+			"*****",
+			"-***-",
+			"*-***",
+			"-****",
+			"**-**",
+			"***-*",
+			"****-",
+			"*-***",
+			"*****",
+			"*****",
+			"-***-",
+			"*-***",
+			"-****",
+			"**-**",
+			"***-*",
+			"****-",
+			"*-***",
+			"*****",
+			"*****",
+			"-***-",
+			"*-***",
+			"-****",
+			"**-**",
+			"***-*",
+			"****-",
+			"*-***",
+			"*****",
+			"*****",
+			"-***-",
+			"-**-*"};
+	//private String [] Bar = new String[] { "-****" };
+	public int x = 0;
+	public int y;
 	private int k = 0;
-	static int SPEED = 4;
+	static int SPEED = 2;
+	boolean D_ISPRESS = false;
+	boolean F_ISPRESS = false;
+	boolean SPACE_ISPRESS = false;
+	boolean J_ISPRESS = false;
+	boolean K_ISPRESS = false;
+	private boolean isClick = true;;
 	
 	public Tabplay(MusicGame musicGame){
 		this.musicGame = musicGame;
@@ -28,20 +71,61 @@ public class Tabplay {
     public boolean hasDotAt(int r, int c) {
         return Bar[r].charAt(c) == '*';
     }
+    
     public void update(float delta){
     	k++;
     }
+    
+    //public boolean checkKey = false;
+    
+    
+    public void checkTap(int r, int c){
+    		if(D_ISPRESS && x == 0 && y<=80 && y>=40){
+    			System.out.println("OK D");
+    		}
+    		else if(D_ISPRESS){
+    			//System.out.println("MISS D");
+    		}
+    		if(F_ISPRESS && x == 120 && y<=80 && y>=40){
+    			System.out.println("OK F");
+    		}
+    		else if(F_ISPRESS){
+    			//System.out.println("MISS F");
+    		}
+    		if(SPACE_ISPRESS && x == 240 && y<=80 && y>=40){
+    			System.out.println("OK SPACE");
+    		}
+    		else if(SPACE_ISPRESS){
+    			//System.out.println("MISS SPACE");
+    		}
+    		if(J_ISPRESS && x == 360 && y<=80 && y>=40){
+    			System.out.println("OK J");
+    		}
+    		else if(J_ISPRESS){
+    			//System.out.println("MISS J");
+    		}
+    		if(K_ISPRESS && x == 480 && y<=80 && y>=40){
+    			System.out.println("OK K");
+    		}
+    		else if(K_ISPRESS){
+    			//System.out.println("MISS K");
+    		}
+    	
+    }
 	
-	public void render(float delta) {
+	public void render(float delta){
 		SpriteBatch batch = musicGame.batch;
         batch.begin();
-    	for(int r = 0; r < 10; r++){
+    	for(int r = 0; r < Bar.length ; r++){
     		for(int c = 0; c < 5; c++){
     			x = (c*120);
     			y = 760 + r*40 - k*SPEED;
     			if(hasBarAt(r,c)){
     				batch.draw(tabplayImg, x, y);
     			}
+    			checkTap(r,c);
+//    			System.out.println(x);
+//    			System.out.println(y);
     		}
     	}
     	update(delta);
